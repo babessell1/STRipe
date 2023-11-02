012b21c43d5a6f349bcd994d8ef490ec6567ee04
babessell1 <babessell@gmail.com>
2023-11-02 13:17:01 -0400

    eh resouces

--- call_eh.bash
+++ call_eh.bash
@@ -6,14 +6,14 @@
 #SBATCH --nodes=1
 #SBATCH --mem=32GB
 #SBATCH --ntasks=11
-#SBATCH --time=6:00:00
-#SBATCH --output=logs/stripe_download.out
-#SBATCH --error=logs/stripe_download.err
+#SBATCH --time=7:59:00
+#SBATCH --output=logs/eh.out
+#SBATCH --error=logs/eh.err
 
 #eval "$(conda shell.bash hook)"
 #conda activate tricolor
 
-#salloc -A remills99 -p standard -N 1 -n 11 -t 6:00:00 --mem=22G
+#salloc -A bioinf593f23_class -p standard -N 1 -n 11 -t 6:00:00 --mem=32G
 
 snakemake -s call_eh.smk --unlock
 snakemake -s call_eh.smk \
