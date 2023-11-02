09683538bc419dce67df4957143956317929e4b6
babessell1 <babessell@gmail.com>
2023-11-02 16:25:28 -0400

    more mem

--- call_eh.bash
+++ call_eh.bash
@@ -13,7 +13,7 @@
 #eval "$(conda shell.bash hook)"
 #conda activate tricolor
 
-#salloc -A bioinf593f23_class -p standard -N 1 -n 11 -t 6:00:00 --mem=32G
+#salloc -A bioinf593f23_class -p standard -N 1 -n 11 -t 6:00:00 --mem=64G
 
 snakemake -s call_eh.smk --unlock
 snakemake -s call_eh.smk \
