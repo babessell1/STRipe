861c4d8ae5f6efa8b08cb17a2c205ab672e88d94
babessell1 <babessell@gmail.com>
2023-11-04 19:47:28 -0400

    refactor bash

--- run_hifi.bash
+++ run_hifi.bash
@@ -18,22 +18,6 @@ snakefiles=("download.smk" "index.smk" "call_trgt.smk")
 # Define your manifest file
 manifest_file="manifests/hifi_manifest.csv"
 
-# Define the configuration file
-config_file="config.yaml"
-
-# Function to delete the temporary manifest and HIFI BAM file
-cleanup() {
-    temp_manifest="$1"
-    sample="$2"
-    ext="$3"
-    
-    rm "$temp_manifest"
-    hifi_bam_file="$DATA_DIR/hifi/${sample}.hifi.bam"
-    if [ -e "$hifi_bam_file" ]; then
-        rm "$hifi_bam_file"
-    fi
-}
-
 # Read the manifest file and process each line
 while IFS=',' read -r sample haplotype file_num datatype url; do
 
@@ -44,25 +28,17 @@ while IFS=',' read -r sample haplotype file_num datatype url; do
     # Create a temporary manifest for the current sample
     temp_manifest="./manifests/temp_manifest.csv"
     echo "$sample,$haplotype,$file_num,$datatype,$url" > "$temp_manifest"
+    hifi_bam_file="/nfs/turbo/dcmb-class/bioinf593/groups/group_05/output/hifi/${sample}.hifi.bam"
 
     # Run each Snakefile with the specified configuration
-    # Run each Snakefile with the specified configuration
-    for snakefile in "${snakefiles[@]}"; do
-        case "$snakefile" in
-            "download.smk")
-                snakemake -s "$snakefile" -c "$config_file" --cores 1 --resources "mem_mb=1000"
-                ;;
-            "index.smk")
-                snakemake -s "$snakefile" -c "$config_file" --cores 1 --resources "mem_mb=4000"
-                ;;
-            "call_trgt.smk")
-                snakemake -s "$snakefile" -c "$config_file" --cores 1 --resources "mem_mb=32000"
-                ;;
-        esac
-    done
+    snakemake -s "download.smk" -c "$config_file" --cores 1 --resources "mem_mb=1000"
+    snakemake -s "index.smk" -c "$config_file" --cores 1 --resources "mem_mb=4000"
+    snakemake -s "call_trgt.smk" -c "$config_file" --cores 1 --resources "mem_mb=32000"
 
-    # Delete the temporary manifest and HIFI BAM file
-    cleanup "$temp_manifest" "$sample" ".bam"
+    # Delete hifi bam file and temp_manifest
+    rm "$hifi_bam_file"
+    rm "$temp_manifest"
+    
 done < "$manifest_file"
 
 
